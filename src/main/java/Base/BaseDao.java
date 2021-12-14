e2787bdb73a2d700f8c88a7dfd84bceca7e7b0f6
Burak <burak.yildiz0@outlook.com>
2021-12-14 19:22:24 +0300

    Add BaseDao class

---
+++ src/main/java/Base/BaseDao.java
@@ -0,0 +1,17 @@
+package Base;
+
+import Hibernate.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class BaseDao {
+    private SessionFactory sessionFactory;
+
+    public BaseDao() {
+        sessionFactory = HibernateUtil.getSessionFactory();
+    }
+
+    protected Session getCurrentSession() {
+        return sessionFactory.openSession();
+    }
+}
