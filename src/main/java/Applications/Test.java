99d782f4b5ba581e64a11854bac285a7a1aac386
Burak <burak.yildiz0@outlook.com>
2021-12-14 22:12:32 +0300

    Add test application

---
+++ src/main/java/Applications/Test.java
@@ -0,0 +1,18 @@
+package Applications;
+
+
+import Entities.Product;
+import EntityServices.ProductEntityService;
+
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        ProductEntityService service = new ProductEntityService();
+        List<Product> productList = service.findAll();
+
+        for(Product product: productList){
+            System.out.println(product);
+        }
+    }
+}
